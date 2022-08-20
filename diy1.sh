f11e2bc917b98c4c3bcdef6c2e19a466f9571a5f
Yvan86 <63568944+Yvan86@users.noreply.github.com>
2022-08-21 00:04:10 +0800

    Delete diy1.sh

--- diy1.sh
+++
@@ -1,9 +0,0 @@
-shopt -s extglob
-rm -rfv !(README.md|diy1.sh)
-shopt -u extglob
-cd ..
-git clone https://github.com/ophub/amlogic-s9xxx-armbian amlogic-s9xxx-armbian1
-cp -rf ./amlogic-s9xxx-armbian1/* ./amlogic-s9xxx-armbian/
-cd ./amlogic-s9xxx-armbian
-rm -rf ./*/.git
-git rm -r --cache *
