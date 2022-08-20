a1505205420f6da4a916a6fd69b00ea2ef844a0d
Yvan86 <63568944+Yvan86@users.noreply.github.com>
2022-08-20 22:18:33 +0800

    Update diy1.sh

--- diy1.sh
+++ diy1.sh
@@ -3,7 +3,7 @@ rm -rfv !(README.md|diy1.sh)
 shopt -u extglob
 cd ..
 git clone https://github.com/ophub/amlogic-s9xxx-armbian amlogic-s9xxx-armbian1
-cp -f ./amlogic-s9xxx-armbian1/* ./amlogic-s9xxx-armbian/
+cp -rf ./amlogic-s9xxx-armbian1/* ./amlogic-s9xxx-armbian/
 cd ./amlogic-s9xxx-armbian
 rm -rf ./*/.git
 git rm -r --cache *
